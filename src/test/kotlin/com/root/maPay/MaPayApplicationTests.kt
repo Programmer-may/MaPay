0f9cddb4e3e3e7859fee46b2e02448246d0c7ca5
JuHyeonPark <parkjh080@naver.com>
2024-09-05 09:23:49 +0900

    init: 프로젝트 셋팅
    프로젝트 셋팅

---
+++ src/test/kotlin/com/root/maPay/MaPayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.root.maPay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaPayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
