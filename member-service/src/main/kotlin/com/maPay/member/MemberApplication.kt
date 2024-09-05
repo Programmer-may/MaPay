4d30bae3365d3d2a2ad8cd1b8c8d3d652cae2c10
JuHyeonPark <parkjh080@naver.com>
2024-09-05 20:10:33 +0900

    feat: member-service 모듈 생성
    member-service 모듈 생성

---
+++ member-service/src/main/kotlin/com/maPay/member/MemberApplication.kt
@@ -0,0 +1,13 @@
+package com.maPay.member
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class MemberApplication
+
+fun main(args: Array<String>) {
+    runApplication<MemberApplication>(*args)
+}
